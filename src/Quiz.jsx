5ab017b9da72b10586a86ab8d5de154a6977cb53
Dimitrije Stojanovic <dimitrije.stojanovic.cu@gmail.com>
2024-06-15 10:00:31 +0200

    Initial Commit

---
+++ src/Quiz.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { resultInitalState } from "./constants";
+
+function Quiz({ questions }) {
+  // HOOOKS
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answerIdx, setAnswerIdx] = useState(null);
+  const [answer, setAnswer] = useState(null);
+  const [result, setResult] = useState(resultInitalState);
+  const [showResult, setShowResult] = useState(false);
+
+  const { question, choices, correctAnswer } = questions[currentQuestion];
+  function onAnswerClick(answer, index) {
+    setAnswerIdx(index);
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  }
+
+  function onClickNext() {
+    setAnswerIdx(null);
+    setResult((prev) =>
+      answer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setCurrentQuestion(0);
+      setShowResult(true);
+    }
+  }
+
+  function onTryAgain() {
+    setResult(resultInitalState);
+    setShowResult(false);
+  }
+
+  return (
+    <div className="quiz-container">
+      {!showResult ? (
+        <>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-question">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                className={answerIdx === index ? "selected-answer" : null}
+                key={answer}
+                onClick={() => onAnswerClick(answer, index)}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIdx === null}>
+              {currentQuestion === questions.length - 1 ? "FINNISHED" : "NEXT"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Questions : <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score : <span>{result.score}</span>
+          </p>
+          <p>
+            Correct Answers : <span>{result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers : <span>{result.wrongAnswers}</span>
+          </p>
+          <button onClick={onTryAgain}>Test Your Knowlegde Again</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
